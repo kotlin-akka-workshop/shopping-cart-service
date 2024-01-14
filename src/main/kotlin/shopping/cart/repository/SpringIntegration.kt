ae4cf2994d4ca08ec372c5478e3d6820f2cd914a
Andrey Somov <public.somov@gmail.com>
2024-01-14 19:27:20 +0400

    Move SpringIntegration to Kotlin

---
+++ src/main/kotlin/shopping/cart/repository/SpringIntegration.kt
@@ -0,0 +1,36 @@
+package shopping.cart.repository
+
+import akka.Done
+import akka.actor.typed.ActorSystem
+import com.typesafe.config.Config
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import java.util.function.*
+
+
+/**
+ * Provides an integration point for initializing a Spring [ApplicationContext] configured for
+ * working with Akka Projections.
+ */
+object SpringIntegration {
+    /**
+     * Returns a Spring [ApplicationContext] configured to provide all the infrastructure
+     * necessary for working with Akka Projections.
+     */
+    @JvmStatic
+    fun applicationContext(system: ActorSystem<*>): ApplicationContext {
+        val config = system.settings().config()
+        val context = AnnotationConfigApplicationContext()
+        // register the Config as a bean, so it can be later injected into SpringConfig
+        context.registerBean(
+            Config::class.java,
+            Supplier { config })
+        context.register(SpringConfig::class.java)
+        context.refresh()
+
+        // Make sure the Spring context is closed when the actor system terminates
+        system.whenTerminated.thenAccept { done: Done? -> context.close() }
+
+        return context
+    }
+}
